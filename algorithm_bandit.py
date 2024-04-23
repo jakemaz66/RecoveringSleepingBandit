3a540826352106b0dd0befb481a0e9c4bcc68b28
jakem <mazurkiewiczj@duq.edu>
2024-04-23 08:02:11 -0400

    Cleaned up algorithm

--- algorithm_bandit.py
+++ algorithm_bandit.py
@@ -4,6 +4,10 @@ import pandas as pd
 import math
 from collections import deque
 import deque
+#Onehot encode eligible
+#np.cumsum()
+#solve with pyspark 
+
 
 def alg(train_df):
     """This function returns the scores for each arm template"""
@@ -21,18 +25,19 @@ def alg(train_df):
     for i in df['selected_template'].unique():
         unique_arms.append(i)
 
-        #Initializing all arm scores as equal (0) to begin
+        #Initializing all arm scores as equal to begin
         arm_score[i] = 0
         arm_eligible_dict.add_key(i)
         arm_selected_dict[i] = []
 
-    subset = df.iloc[:5000, :]
+
+    subset = df.iloc[: :]
 
     #Iterating through each unique round [t], which is same as each row in dataframe
     for index, row in subset.iterrows():
         print(index)
 
-        #Selecting arm 
+        #Selecting arm and adding arm probability to historical rounds where arm selected
         arm = row['selected_template']
         
         #Store row index for selected arm
@@ -42,49 +47,61 @@ def alg(train_df):
         for eligible in row['eligible_templates']:
                 arm_eligible_dict.add_value(eligible, index)
 
-        #Get all previous rows where arm selected and apply reward function to calculate mu plus
         rows_with_arm = df.loc[arm_selected_dict[arm]]
+        #Reward function application
         selected = len((rows_with_arm[rows_with_arm['session_end_completed'] == 1]))
-        #Multiplying arm score by reward function
-        selected = selected * arm_score[arm]
+
+        mu_plus = selected / len(rows_with_arm)
 
         #Get all previous rows where arm eligible but not selected and apply reward function to get mu minus
         rows_with_eligible = df.loc[arm_eligible_dict.get_values(arm)]
 
-        #Rows where eligible
-        total = len(rows_with_eligible)
-
-        if (selected == 0):
-            selected = 0.0001
-        if (total == 0):
-            total = 0.0001
-
-        mu_plus = selected / total
-
-        #C
         not_selected_indices = list(set(rows_with_eligible.index) - set(rows_with_arm.index))
         rows_eligible_not_selected = df.loc[not_selected_indices]
 
+        #Reward function application
         selected_minus = len((rows_eligible_not_selected[rows_eligible_not_selected['session_end_completed'] == 1]))
-        selected_minus = (1-arm_score[arm])**-1 * selected_minus
 
-        total_minus = len(rows_eligible_not_selected)
+        leng = len(rows_eligible_not_selected)
 
-        if selected_minus == 0:
-            selected_minus = 0.0001
-        if total_minus == 0:
-            total_minus = 0.0001
-   
-        mu_minus = selected_minus / total_minus
+        if (len(rows_eligible_not_selected) == 0):
+            leng = 1
+ 
+        mu_minus = selected_minus / leng
 
         #Handling divide by 0 cases
-        if (mu_plus == 0):
-            mu_plus = 0.0001
         if (mu_minus == 0):
-            mu_minus = 0.0001
+            mu_minus = 1
 
         #Relative difference calculation to get the arm score
         score = ((mu_plus-mu_minus)/mu_minus)
+
+        #Updating arm score in arm_score dictionary
+        arm_score[arm] = score
+ 
+    return arm_score, unique_arms  
+
+ 
+
+def eval(test_df, train_df):
+    df = test_df
+    #Number of rounds in test dataset
+    t = len(df)
+
+    #Retrieving arm scores from training data
+    arm_score, unique = alg(train_df)
+
+    #Assigning each arm an equal random chance to be chosen, the old policy
+    decay_arm_score = arm_score.copy()
+
+    #Defining selection probabilities
+    soft_max_prob = {}
+    total_sum = 0
+
+    subset = df.iloc[:, :]
+
+    for index, row in df.iterrows():
+        arm = row['selected_template']
 
         #Getting days since arm a last chosen from history column to retrieve d (days) for recency penalty
         for i in row['history'][::-1]:
@@ -95,57 +112,32 @@ def alg(train_df):
                 #If arm not in history, set default days to 34
                 days = 34
                 
-        #Adding decay function to score for recency effect
-        score = score - (0.017*0.5)**(days/15)
+        #Adding decay function to score for recency effect, if this arm was recently chosen we want to lower it's prob for the round
+        decay_arm_score[arm] = arm_score[arm] - (0.017*0.5)**(days/15)
 
-        #Updating arm score in arm_score dictionary
-        arm_score[arm] = score
-        
-    return arm_score, unique_arms   
+        # Computing softmax of arm scores [π (a|t)]
+        new_policies = {}
+        soft_max_prob = {}
+        old_policy = {}
 
+        for eligible in row['eligible_templates']:
+            old_policy[eligible] = (1/len(row['eligible_templates']))
+            soft_max_prob[eligible] = math.exp(decay_arm_score[eligible])
 
-def newpolicy_score(test_df, train_df):
-    """This function returns the difference between the new policy and the old"""
-
-    df = test_df
-
-    #Number of rounds in test dataset
-    t = len(df)
-
-    #Retrieving arm scores from training data
-    arm_score, unique = alg(train_df)
-    filtered_dict = {key: value for key, value in arm_score.items() if value != 0}
-
-    #Assigning each arm an equal random chance to be chosen, the old policy
-    old_policy = {}
-    old_policy = {arm: 1/len(unique) for arm in unique}
-
-    #Defining selection probabilities
-    soft_max_prob = {}
-
-    # Computing softmax of arm scores [π (a|t)], add an explore hyperparameter to divide by
-    explore=1.1
-    for eligible in filtered_dict.keys():
-        soft_max_prob[eligible] = math.exp(filtered_dict[eligible]/explore)
-
-    #Calculating new policy
-    divided_values = [value / explore for value in soft_max_prob.values()]
-    sum_probs = sum(divided_values)
-
-    new_policies = {arm: soft_max_prob[arm] / sum_probs for arm in soft_max_prob}
+            #Calculating new policy
+            sum_probs = sum(soft_max_prob.values())
+            new_policies = {arm: soft_max_prob[arm] / sum_probs for arm in soft_max_prob}
     
-    #Getting differences in policy weights for each arm
-    diff = {key: new_policies[key] / old_policy.get(key, 0) for key in new_policies}
+        #Getting differences in policy weights for each arm
+        diff = {key: new_policies[key] / old_policy.get(key, 0) for key in new_policies}
 
-   #Iterate through the DataFrame and calculate results, multiplying new reward by difference in policies (new weights)
-    total_sum = 0
-    for index, row in df.iterrows():
         if row['session_end_completed'] == 1:
-            template = row['selected_template']
-            value_to_multiply = diff.get(template, 0)
+            value_to_multiply = diff.get(arm, 0)
             result = row['session_end_completed'] * value_to_multiply
             df.at[index, 'result'] = result
             total_sum += result
+
+        decay_arm_score = arm_score.copy()
 
     #Calculating reward of new behavior policy
     new_reward = ((1/t)* total_sum)
@@ -159,6 +151,7 @@ def newpolicy_score(test_df, train_df):
 
     return rel_diff
 
+
 if __name__ == '__main__':
-    newpolicy_score(parquet_reader.DataReader('data/test1.snappy.parquet', 500, 5000).read(),
-                    parquet_reader.DataReader('data/train1.snappy.parquet', 500, 100000).read())
+    eval(parquet_reader.DataReader('data/test1.snappy.parquet', 200, 1400).read(),
+                    parquet_reader.DataReader('data/train1.snappy.parquet', 500, 1001).read())
